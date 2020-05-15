815b9e467b0380b1d8d6c6b96229be3d8f8b8ddd
AndreasEhret <andreas.ehret@karakun.com>
2020-05-15 13:28:09 +0200

    return empty string as long as vendor is not set

--- openwebstart/src/main/java/com/openwebstart/jvm/ui/dialogs/ConfigurationDialog.java
+++ openwebstart/src/main/java/com/openwebstart/jvm/ui/dialogs/ConfigurationDialog.java
@@ -254,7 +254,10 @@ public class ConfigurationDialog extends ModalDialog {
         }
 
         private String displayStringForVendor(Vendor vendor) {
-            if (Objects.isNull(vendor) || Objects.equals(vendor, Vendor.ANY_VENDOR)) {
+            if (Objects.isNull(vendor)) {
+                return "";
+            }
+            if (Objects.equals(vendor, Vendor.ANY_VENDOR)) {
                 return ANY_VENDOR_DISPLAY_NAME;
             }
             return vendor.getName();
